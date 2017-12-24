63722de178fc16f2d7634512fc48bb0c58fffe26
sharpwaveripple <jonathantay193@gmail.com>
2017-12-24 18:10:30 +0000

    modified scripts

--- scripts/for_publication.R
+++ scripts/for_publication.R
@@ -1,103 +1,162 @@
 library(tidyverse)
 library(broom)
+library(rpart)
+library(pROC)
 
-extractP <- function(results) {
-  p <- results[seq(2, length(results), 2)]
-  p <- unlist(p)
-  corr <- p.adjust(p)
-  return(corr)
-}
-
-calc.odds.ratios <- function(logistic.formula, df) {
-  sig.digits <- 3
-  format.str <- paste("%.", sig.digits, "f", sep = "")
-  logit <- glm(formula(logistic.formula), data = df, family = binomial)
-  logit.df <- tidy(logit)
-  logit.df %>%
-    mutate(or = exp(estimate),
-           var.diag = diag(vcov(logit)),
-           or.se = sqrt(or^2 * var.diag)) -> logit.df
-  results <- round(cbind(logit.df$or, 
-                         logit.df$or.se, 
-                         exp(confint(logit)), 
-                         logit.df$p.value), sig.digits)
-  results.format <- matrix(data = sprintf(format.str, results),
-                           nrow = dim(results)[1],
-                           ncol = dim(results)[2],
-                           dimnames = list(rownames(results),
-                                           c("Odds ratio",
-                                             "Standard error",
-                                             "2.5%", "97.5%", "p")))
-  results.table <- as.table(results.format)
-  return(results.table)
+correct_p <- function(pMat, method) {
+  pMat %>%
+    unlist %>%
+    p.adjust(method = method) -> pVec
+  dim(pVec) <- dim(pMat)
+  dimnames(pVec) <- dimnames(pMat)
+  return(pVec)
 }
 
 bin <- c("Sex", "Diabetes", "HF", "MI/Angina", "Side")
 ord <- c("mRS_3m", "occl4", "pig3")
-cont <- c("Age", "BslNIH", "tPAdelay",
-          "CoreCBF30", "MismCBF30", "CoreCBV60", "MismCBV60")
+cont <- c("Age",
+          "BslNIH",
+          "tPAdelay",
+          "CoreCBF30",
+          "MismCBF30",
+          "CoreCBV60",
+          "MismCBV60")
 vars <- c(bin, ord, cont)
+sig <- 3
 
 read_csv('../data/data_final.csv', na = "?") %>%
   select(vars) %>%
   drop_na(., BslNIH) %>%
-  mutate(poor = mRS_3m >= 5,
-         moderate = mRS_3m <= 3,
-         good = mRS_3m <= 2,
-         excellent = mRS_3m <= 1) %>%
+  mutate(
+    full = mRS_3m,
+    dead = mRS_3m == 6,
+    poor = mRS_3m >= 5,
+    moderate = mRS_3m <= 3,
+    good = mRS_3m <= 2,
+    excellent = mRS_3m <= 1
+  ) %>%
   mutate_at(bin, funs(factor(.))) %>%
   mutate_at(ord, funs(ordered(.))) ->
   df
-  
-  
-summarise_at(cont, c("median", "IQR"), na.rm=T)
+
+df %>%
+  summarise_at(cont, c("median", "IQR"), na.rm = T)
 
 df %>%
   count(mRS_3m) %>%
-  mutate(per=round(n/sum(n), 3)*100) -> clinical
+  mutate(per = round(n / sum(n), sig) * 100) -> clinical
+
+
+ord = ord[!ord %in% 'mRS_3m'] # gracelessly pop off mRS
 
 catVars <- c(bin, ord)
-outVars <- c('poor', 'moderate', 'good', 'excellent')
+outVars <- c('dead', 'poor')#, 'moderate', 'good', 'excellent')
 
-catResults <- matrix(, nrow=length(catVars), ncol = 0)
-contResults <- matrix(, nrow=length(cont), ncol = 0)
+
+catResults <- matrix(, nrow = length(catVars), ncol = 0)
+contResults <- matrix(, nrow = length(cont), ncol = 0)
 rownames(catResults) <- catVars
 rownames(contResults) <- cont
 
 for (outcome in outVars) {
-  map(df[catVars], function(x) table(x, df[[outcome]])) %>%
-  map(., function(x) chisq.test(x)) %>%
-  map_df(., magrittr::extract, c('statistic', 'p.value')) %>%
-  round(., 3) -> x2
+  map(df[catVars], function(x)
+    table(x, df[[outcome]])) %>%
+    map(., function(x)
+      chisq.test(x)) %>%
+    map_df(., magrittr::extract, c('statistic', 'p.value')) -> x2
   catResults <- cbind(catResults, x2)
-
-  map(df[cont], function(x) t.test(x ~ df[[outcome]])) %>%
-  map_df(., magrittr::extract, c('statistic', 'p.value')) %>%
-  round(., 3) -> t
-  contResults <- cbind(contResults, t)
   
-  df %>%
-    when(
-      outcome=
-    )
+  map(df[cont], function(x)
+    t.test(x ~ df[[outcome]])) %>%
+    map_df(., magrittr::extract, c('statistic', 'p.value')) -> f
+  contResults <- cbind(contResults, f)
 }
 
+map(df[catVars], function(x)
+  table(x, df$mRS_3m)) %>%
+  map(., function(x)
+    chisq.test(x)) %>%
+  map_df(., magrittr::extract, c('statistic', 'p.value')) -> x2
+catResults <- cbind(catResults, x2)
 
-map(df[catVars], function(x) table(x, df$mRS_3m)) %>%
-map(., function(x) chisq.test(x)) %>%
-map_df(., magrittr::extract, c('statistic', 'p.value')) %>%
-round(., 3) -> x2
+map(df[cont], function(x)
+  aov(x ~ df$mRS_3m)) %>%
+  map(., function(x)
+    tidy(x)) %>%
+  map_df(., magrittr::extract, c('statistic', 'p.value')) %>%
+  drop_na -> f
+contResults <- cbind(contResults, f)
 
-map(df[contVars], function(x) aov(x ~ df$mRS_3m)) %>%
-map_df(., magrittr::extract, c('statistic', 'p.value')) %>%
-round(., 3) -> t
-
-x2p <- extractP(catResults)
-tp <- extractP(contResults)
-
+uni <- rbind(catResults, contResults)
+uni_stats <- uni[seq(1, length(uni), 2)]
+uni_ps <- uni[seq(2, length(uni), 2)]
+colnames(uni_ps) <- c(outVars, 'full')
+x = correct_p(uni_ps, method = 'holm')
 
 for (outcome in outVars) {
-  logistic.formula <- paste(outcome, "Age + BslNIH + occl4 + pig3 + CoreCBF30 + MismCBF30",
-                            sep = " ~ ")
-  results <- calc.odds.ratios(logistic.formula, df)
+  paste(outcome, "Age + BslNIH + occl4 + pig3 + CoreCBF30 + MismCBF30",
+        sep = " ~ ") %>%
+    glm(., data = df, family = binomial) ->
+    logit
+  
+  logit %>%
+    confint %>% 
+    exp ->
+    ci
+  
+  logit %>%
+    tidy() %>%
+    mutate(or = exp(estimate)) %>%
+    select(or, p.value) %>%
+    cbind(., ci) ->
+    results
+  
+  #dplyr rearrange columns
 }
+
+cartVars <- c("CoreCBF30", "CoreCBV60", "Age", "BslNIH")
+outVars <- c(outVars, 'full')
+
+for (outcome in outVars) {
+  paste(outcome, cartVars, sep="~") %>%
+    map(as.formula) %>%
+    map(rpart, data=df) %>%
+    map(magrittr::extract('splits')) %>%
+    map(as.tibble) %>%
+    map(., function(x) select(x, 'index')) %>%
+    map(., function(x) slice(x, 1)) %>%
+    map(as.numeric) %>%
+    unlist ->
+    splits
+  
+  newnames <- paste(cartVars, "bin", sep="_")
+  
+  df %>%
+    select(cartVars) < splits -> 
+    splitVars
+  
+  colnames(splitVars) <- newnames
+  
+  temp <- cbind(df, splitVars)
+  
+  paste(outcome, "CoreCBV60_bin + Age_bin + BslNIH_bin",
+        sep="~") %>%
+    as.formula %>%
+    glm(., data=temp, family=binomial) ->
+    logit
+
+    predict(., type="response") ->
+    predVals
+  
+  temp$prob <- predVals
+  roc(as.factor(temp$poor), temp$prob)
+    
+}
+
+x <- rpart(poor ~ CoreCBF30, data = df)
+x$splits %>% as.tibble() %>% select(index) -> y
+slice(y, 1) %>% as.numeric
+
+df %>% mutate(binCore = CoreCBF30 < 66.85) -> test
+x2 <- table(test$binCore, test$poor)
+# plot his of df$occl4
