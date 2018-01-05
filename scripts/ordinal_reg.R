4ba4a32a97683eefab3ee5daa987be0aa1bddd75
sharpwaveripple <jonathantay193@gmail.com>
2018-01-04 22:12:12 -0500

    exp() ordinal results

--- scripts/ordinal_reg.R
+++ scripts/ordinal_reg.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(ordinal)
 
 datafile <- "../data/data_final.csv"
@@ -8,20 +9,21 @@ iv <- c("CoreCBF30", "CoreCBV60")
 mism.cov <- c("MismCBF30", "MismCBV60")
 all.vars <- c(iv, static.cov, dv, mism.cov)
 
+ord <- c("mRS_3m", "occl4", "pig3")
+read_csv(datafile, na="?") %>%
+  select(all.vars) %>%
+  drop_na -> df
 
-
-data.raw <- read.csv(datafile, header = T, na.strings = "?")
-df <- data.raw[all.vars]
-df.final <- df[complete.cases(df), ]
-df.final$mRS_3m <- ordered(df.final$mRS_3m)
-df.final$occl4 <- ordered(df.final$occl4)
-df.final$pig3 <- ordered(df.final$pig3)
+df[ord] <- lapply(df[ord], ordered)
 
 for (i in iv) {
  for (j in mism.cov) {
     model.vars <- c(i, j, static.cov)
     ordinal.formula <- paste("mRS_3m ~", paste(model.vars, collapse = " + "))
-    fit <- clm(formula(ordinal.formula), data=df.final, Hess=T)
-    print(summary(fit))
+    fit <- clm(formula(ordinal.formula), data=df, Hess=T)
+    print(exp(coef(fit)[-(1:6)]))
+    print(exp(confint(fit)))
+    print("=========")
  }
 }
+
